2f6c4b6f1a537ccb84a7530e67ae2879cd34acab
Nazreen <qureshi992005@gmail.com>
2026-02-28 23:05:45 +0530

    Update React counter project

--- 01counterReact/src/App.jsx
+++ 01counterReact/src/App.jsx
@@ -13,8 +13,22 @@ function App() {
     //  counter = counter + 1 // console par  to update hote dikhega lekin UI  me update nahi hoga 
      // so react will handle what updation will happen fir this they have prvided some methods -> hooks 
      //data will updates through these hooks
-      { setCount(counter + 1)
-       console.log("clicked" , counter)}
+      setCount(counter + 1)
+      setCount(counter + 1)
+       setCount(counter + 1)
+       setCount(counter + 1)
+
+        // value will update 4 times but in UI it will update only once because react will batch all the updates and then update the UI once
+        // value 16 hi aayegi 
+        //as setcounter me ek call back milta hai
+        //usesate sabhi updates ko batch karta hai aur fir UI ko update karta hai to charo same counter ko update kar raha hai that mean it will consider as same hi to kaam hai to ek hi baar update karega
+
+        setCount((prevCounter) => prevCounter + 1) // this is the correct way to update the state when we have multiple updates in a single function
+        setCount((prevCounter) => prevCounter + 1)
+        setCount((prevCounter) => prevCounter + 1)
+        setCount((prevCounter) => prevCounter + 1)
+      
+        console.log("clicked" , counter)
    }
   
    const removeValue = () =>{
@@ -23,7 +37,7 @@ function App() {
    }
   return (
     <>
-      <h1>Counting with React</h1>
+      <h1 className='bg-red '>Counting with React</h1>
       <h2>Counter Value : {counter}</h2> 
 
       <button onClick={addValue}>Add Value {counter}</button>
